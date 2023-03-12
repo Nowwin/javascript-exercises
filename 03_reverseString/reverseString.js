8d3dd36286e3f038d25f3c11e4de5afb62ee0781
Abhijeet Chowdhury <abhijeetchowdhury1993@gmail.com>
2023-03-12 00:55:00 -0500

    Implement the solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(message) {
+    result = "";
+    lenOfString = message.length;
 
+    for (let i = 0; i < lenOfString; i++) {
+        result = result 
+        + message.slice(lenOfString - 1 - i, lenOfString - i);
+    }
+
+    return result;
 };
 
 // Do not edit below this line
