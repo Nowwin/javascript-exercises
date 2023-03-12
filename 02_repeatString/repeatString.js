7106748c28793d2e2dfe513d3038cc30ff8a0241
Abhijeet Chowdhury <abhijeetchowdhury1993@gmail.com>
2023-03-12 00:23:39 -0500

    Implement the final solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(message, num) {
 
+    if (num < 0) {
+        return 'ERROR';
+    }
+
+    result = "";
+
+    for (let index = 0; index < num; index++) {
+        result = result + message;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
